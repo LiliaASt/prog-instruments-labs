78887b4761a81cf3065712b2097c795ee77d4c31
LiliaASt <lastsatryan2704@mail.ru>
2024-12-15 16:57:21 +0300

    upgrade

--- lab_3/path.py
+++ lab_3/path.py
@@ -1,13 +1,13 @@
 CVS_PATH = 'C:/Users/lasts/projects/prog-instruments-labs/lab_3/3.csv'
 RESULT_PATH = 'C:/Users/lasts/projects/prog-instruments-labs/lab_3/result.json'
 
-VALID_EMAIL_REGEX = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$'
-VALID_HEIGHT_REGEX = r'^[1-2].d{2}$'
-VALID_SNILS_REGEX = r'^d{11}$'
-VALID_PASSPORT_REGEX = r'^d{2} d{2} d{6}$'
-VALID_OCCUPATION_REGEX = r'^[ws]+$'
-VALID_LONGITUDE_REGEX = r'^-?d+.d+$'
-VALID_HEX_COLOR_REGEX = r'^#[0-9a-fA-F]{6}$'
-VALID_ISSN_REGEX = r'^d{4}-d{4}$'
-VALID_LOCALE_CODE_REGEX = r'^[a-z]{2,3}(-[a-z]{2})?$'
-VALID_TIME_REGEX = r'^(?:2[0-3]|[0-1]d):[0-5]d:[0-5]d.d{6}$'
+VALID_EMAIL_REGEX = r"^[a-zA-Z0-9]+(?:[._][a-zA-Z0-9]+)*\@[a-zA-Z]+(?:\.[a-zA-Z]+)+$"
+VALID_HEIGHT_REGEX = r"^[1-2]\.\d{2}$"
+VALID_SNILS_REGEX = r"^\d{11}$"
+VALID_PASSPORT_REGEX = r"^\d{2}\s\d{2}\s\d{6}$"
+VALID_OCCUPATION_REGEX = r"[-a-zA-Zа-яА-Я]+"
+VALID_LONGITUDE_REGEX = r"^-?\d{1,3}\.\d{0,}$"
+VALID_HEX_COLOR_REGEX = r"^\#[0-9a-fA-F]{6}$"
+VALID_ISSN_REGEX = r"^\d{4}-\d{4}$"
+VALID_LOCALE_CODE_REGEX = r"^[a-z]{1,3}-?[a-z]{0,3}$"
+VALID_TIME_REGEX = r"^(2[0-3]|[0-1]\d):[0-5]\d:[0-5]\d\.\d{6}$"
